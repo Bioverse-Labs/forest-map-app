055a1cf53dd662f1db03a2d8343cb912b18bf83d
Giovanny Piovesan <giowrp@gmail.com>
2020-09-24 16:43:56 -0300

    feat: added map home screen

---
+++ lib/screens/map.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({Key key}) : super(key: key);
+
+  final Completer<GoogleMapController> _controller = Completer();
+
+  final _initialPosition = CameraPosition(
+    target: LatLng(-3.4653052, -62.2246353),
+    zoom: 14.4746,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Forest Map App'),
+      ),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _initialPosition,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+    );
+  }
+}
