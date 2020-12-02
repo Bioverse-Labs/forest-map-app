1215b316f9fda5deb503c9d208c66f3892265a14
Giovanny Piovesan <giowrp@gmail.com>
2020-12-01 21:18:40 -0300

    chore: added firebase basic config

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:forestMapApp/screens/map.dart';
 
@@ -6,13 +7,20 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+  MyApp({Key key}) : super(key: key);
+
+  final _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Forest App Map',
-      home: HomeScreen(),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        return MaterialApp(
+          title: 'Forest App Map',
+          home: HomeScreen(),
+        );
+      },
     );
   }
 }
--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,29 +1,17 @@
 import 'dart:async';
 
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   HomeScreen({Key key}) : super(key: key);
 
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
   final Completer<GoogleMapController> _controller = Completer();
 
   final _initialPosition = CameraPosition(
     target: LatLng(-3.4653052, -62.2246353),
     zoom: 14.4746,
   );
-
-  @override
-  void initState() {
-    Firebase.initializeApp();
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
