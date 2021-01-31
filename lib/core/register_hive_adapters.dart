ca8c2437ad2e859f61a04dedc8297f7f0e8d0ea2
Giovanny Piovesan <giowrp@gmail.com>
2021-01-31 03:43:51 -0300

    removed unsed files

--- lib/core/register_hive_adapters.dart
+++
@@ -1,8 +0,0 @@
-import '../models/hive/geopoint.dart';
-import '../models/hive/tree.dart';
-import 'package:hive/hive.dart';
-
-void registerHiveAdapters() {
-  Hive.registerAdapter(GeopointHiveAdapter());
-  Hive.registerAdapter(TreeHiveAdapter());
-}
--- lib/core/register_singletons.dart
+++
@@ -1,8 +0,0 @@
-import 'navigation/app_navigator.dart';
-import 'package:get_it/get_it.dart';
-import 'package:image_picker/image_picker.dart';
-
-void registerSingletons() {
-  GetIt.I.registerLazySingleton<AppNavigator>(() => AppNavigator());
-  GetIt.I.registerLazySingleton<ImagePicker>(() => ImagePicker());
-}
