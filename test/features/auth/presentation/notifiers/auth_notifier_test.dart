2f71c387742a3ee051551d4bf67013b2097e5361
Giovanny Piovesan <giowrp@gmail.com>
2021-02-03 03:12:15 -0300

    test: add test to setUser method of authNotifier

--- test/features/auth/presentation/notifiers/auth_notifier_test.dart
+++ test/features/auth/presentation/notifiers/auth_notifier_test.dart
@@ -195,4 +195,22 @@ void main() {
       verifyNoMoreInteractions(mockSignUp);
     });
   });
+
+  group('setUser', () {
+    test(
+      'should set [User] into auth notifier',
+      () async {
+        await expectToNotifiyListener<AuthNotifierImpl>(
+          authNotifierImpl,
+          () => authNotifierImpl.setUser(tUser),
+          [
+            NotifierAssertParams(
+              value: (notifier) => notifier.user,
+              matcher: tUser,
+            ),
+          ],
+        );
+      },
+    );
+  });
 }
